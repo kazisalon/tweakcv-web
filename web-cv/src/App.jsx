b372519c637338e85b1d15bc753b6b90d7b7b057
kazisalon <kazisawlon@gmail.com>
2025-04-24 19:30:13 +0545

    frontend added

---
+++ web-cv/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <div className="header-content">
+          <h1 className="header-title">My Application</h1>
+        </div>
+      </header>
+      <main>
+        <div className="main-content">
+          <div className="content-box">
+            <p className="content-text">Main content area</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
